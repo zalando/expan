f4b64e25f31487efe92d8bf72896d62300057972
Robert Muil <robert.muil@zalando.de>
2016-05-16 13:18:31 +0200

    Bump version: 0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class build_ext(_build_ext):
 
 setup(
 	name='expan',
-	version='0.2.3',
+	version='0.2.4',
 	description="Experiment Analysis Library",
 	long_description=readme + '\n\n' + history,
 	author="Zalando SE",
