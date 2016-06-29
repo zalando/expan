5a357581f2eb8cf8ea64f77b8f75936363983c87
Jie Bao <jie.bao@zalando.de>
2016-06-29 15:47:47 +0200

    prob function works for >2 variants

--- expan/core/results.py
+++ expan/core/results.py
@@ -303,20 +303,23 @@ def prob_uplift_over_zero_single_metric(result_df, baseline_variant):
 	Returns:
 		DataFrame: result data frame with one additional statistic 'prob_uplift_over_0'
 	"""
-	n1,n2 = np.array(result_df.xs(('sample_size'),level=('statistic'))).flatten()
 	pctile = 97.5 # result should be independent of the percentile that we choose
 	all_variants = set(result_df.columns.levels[1])
+	# iterate over all non-baseline variants
 	variant = (all_variants - set([baseline_variant])).pop()
-	mu = float(result_df.xs(('uplift'),level=('statistic'))[('value',variant)])
-	x = float(result_df.xs(('uplift_pctile',pctile),level=('statistic','pctile'))[('value',variant)])
-	sigma = statx.estimate_std(x, mu, pctile)
+	prob_dict = {baseline_variant:np.nan}
+	for v in variant:
+		mu = float(result_df.xs(('uplift'),level=('statistic'))[('value',v)])
+		x = float(result_df.xs(('uplift_pctile',pctile),level=('statistic','pctile'))[('value',v)])
+		sigma = statx.estimate_std(x, mu, pctile)
 
-	prob = 1 - norm.cdf(0, loc=mu, scale=sigma)
-	prob_dict = {baseline_variant:np.nan, variant:prob}
+		prob = 1 - norm.cdf(0, loc=mu, scale=sigma)
+		prob_dict[v] = prob
+
+	# convert dict to df
 	prob_list = []
 	for v in result_df.columns.levels[1]:
 		prob_list.append(prob_dict[v])
-
 	# a prob df w/o multi-index
 	prob_df = pd.DataFrame([prob_list], columns=result_df.columns)
 	# reconstruct indices
