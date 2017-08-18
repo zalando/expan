d74e600b633a479163ba8040220378966f8105c2
Shan Huang <schan.huang@gmail.com>
2017-08-18 11:35:42 +0200

    Initialize empty list inside func

--- expan/core/experiment.py
+++ expan/core/experiment.py
@@ -31,7 +31,13 @@ class Experiment(object):
     """
     Class which adds the analysis functions to experimental data.
     """
-    def __init__(self, control_variant_name, data, metadata, report_kpi_names=[], derived_kpis=[]):
+    def __init__(self, control_variant_name, data, metadata, report_kpi_names=None, derived_kpis=None):
+        if report_kpi_names is None:
+            report_kpi_names = list()
+
+        if derived_kpis is None:
+            derived_kpis = list()
+
         experiment_column_names = set(['entity', 'variant'])
         numerical_column_names  = set(get_column_names_by_type(data, np.number))
 
