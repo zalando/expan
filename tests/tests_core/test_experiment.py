ce1a16271b869871d06d35a93377e34c3e55cf90
mkolarek <marko.kolarek@zalando.de>
2017-08-24 13:48:11 +0200

    OCTO-1930 re-enabled bayesian unit tests

--- tests/tests_core/test_experiment.py
+++ tests/tests_core/test_experiment.py
@@ -176,7 +176,7 @@ class ExperimentClassTestCases(ExperimentTestCase):
                                                       self.derived_kpi_2['name']],
                            [self.derived_kpi_1, self.derived_kpi_2]).delta('group_sequential')
 
-    @unittest.skip("sometimes takes too much time")
+    # @unittest.skip("sometimes takes too much time")
     def test_bayes_factor_delta(self):
         ndecimals = 5
         res = self.getExperiment(['normal_same']).delta(method='bayes_factor', num_iters=2000)
@@ -200,12 +200,12 @@ class ExperimentClassTestCases(ExperimentTestCase):
         self.assertNumericalEqual(aStats['treatment_mean'],  0.025219, ndecimals)
         self.assertNumericalEqual(aStats['control_mean'],   -0.007833, ndecimals)
 
-    @unittest.skip("sometimes takes too much time")
+    # @unittest.skip("sometimes takes too much time")
     def test_bayes_factor_delta_derived_kpis(self):
         exp = self.getExperiment([self.derived_kpi_1['name']], [self.derived_kpi_1])
         res = exp.delta(method='bayes_factor', num_iters=2000)
 
-    @unittest.skip("sometimes takes too much time")
+    # @unittest.skip("sometimes takes too much time")
     def test_bayes_precision_delta(self):
         ndecimals = 5
         res = self.getExperiment(['normal_same']).delta(method='bayes_precision', num_iters=2000)
@@ -229,7 +229,7 @@ class ExperimentClassTestCases(ExperimentTestCase):
         self.assertNumericalEqual(aStats['treatment_mean'],  0.025219, ndecimals)
         self.assertNumericalEqual(aStats['control_mean'],   -0.007833, ndecimals)
 
-    @unittest.skip("sometimes takes too much time")
+    # @unittest.skip("sometimes takes too much time")
     def test_bayes_precision_delta_derived_kpis(self):
         exp = self.getExperiment([self.derived_kpi_1['name']], [self.derived_kpi_1])
         res = exp.delta(method='bayes_precision', num_iters=2000)
