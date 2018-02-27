fixed p value calculation call and adapt unit tests

expan/core/early_stopping.py
@@ -113,7 +113,7 @@ def group_sequential(x, y, spending_function='obrien_fleming', estimated_sample_
     treatment_statistics = SampleStatistics(int(n_x), float(np.nanmean(_x)), float(np.nanvar(_x)))
     control_statistics   = SampleStatistics(int(n_y), float(np.nanmean(_y)), float(np.nanvar(_y)))
     variant_statistics   = BaseTestStatistics(control_statistics, treatment_statistics)
-    p_value              = statx.compute_p_value(_x, _y, int(n_x), int(n_y))
+    p_value              = statx.compute_p_value_from_samples(_x, _y)
     statistical_power    = statx.compute_statistical_power_from_samples(_x, _y, alpha)
 
     logger.info("Group sequential analysis finished!")
@@ -364,7 +364,7 @@ def get_or_compile_stan_model(model_file, distribution):
     """
     python_version = '{0[0]}.{0[1]}'.format(sys.version_info)
     compiled_model_file = tempfile.gettempdir() + '/expan_early_stop_compiled_stan_model_' \
-                          + distribution + '_' + python_version + '.pkl'
+                                                + distribution + '_' + python_version + '.pkl'
 
     if os.path.isfile(compiled_model_file):
         sm = pickle.load(open(compiled_model_file, 'rb'))

tests/tests_core/test_early_stopping.py
@@ -7,23 +7,20 @@ from expan.core.util import find_list_of_dicts_element
 
 
 class EarlyStoppingTestCase(unittest.TestCase):
-    """
-      Defines the setUp() and tearDown() functions for the early-stopping test cases.
-      """
+    """ Defines the setUp() and tearDown() functions for the early-stopping test cases."""
 
     def setUp(self):
-        """
-        Load the needed datasets for all EarlyStoppingTestCases and set the random
+        """ Load the needed datasets for all EarlyStoppingTestCases and set the random
         seed so that randomized algorithms show deterministic behaviour.
         """
         np.random.seed(0)
 
-        self.rand_s1 = np.random.normal (loc=0.0, size=1000)
-        self.rand_s2 = np.random.normal (loc=0.1, size=1000)
-        self.rand_s3 = np.random.poisson(lam=1.0, size=1000)
-        self.rand_s4 = np.random.poisson(lam=3.0, size=1000)
-        self.rand_s5 = np.random.normal (loc=0.0, size=1000)
-        self.rand_s6 = np.random.normal (loc=0.1, size=1000)
+        self.rand_s1 = list(np.random.normal (loc=0.0, size=1000))
+        self.rand_s2 = list(np.random.normal (loc=0.1, size=1000))
+        self.rand_s3 = list(np.random.poisson(lam=1.0, size=1000))
+        self.rand_s4 = list(np.random.poisson(lam=3.0, size=1000))
+        self.rand_s5 = list(np.random.normal (loc=0.0, size=1000))
+        self.rand_s6 = list(np.random.normal (loc=0.1, size=1000))
         self.rand_s5[0] = np.nan
         self.rand_s6[0] = np.nan
         self.rand_s6[1] = np.nan
@@ -37,14 +34,10 @@ class EarlyStoppingTestCase(unittest.TestCase):
 
 
 class GroupSequentialTestCases(EarlyStoppingTestCase):
-    """
-      Test cases for the group sequential functions in core.early_stopping.
-      """
+    """ Test cases for the group sequential functions in core.early_stopping."""
 
     def test_obrien_fleming(self):
-        """
-        Check the O'Brien-Fleming spending function.
-        """
+        """ Check the O'Brien-Fleming spending function."""
         # Check array as input
         res = es.obrien_fleming(np.linspace(0, 1, 5 + 1)[1:])
         expected_res = [1.17264468e-05, 1.94191300e-03, 1.13964185e-02, 2.84296308e-02, 5.00000000e-02]
@@ -60,101 +53,90 @@ class GroupSequentialTestCases(EarlyStoppingTestCase):
         """
         res = es.group_sequential(self.rand_s1, self.rand_s2)
 
-        self.assertEqual               (res['stop'],                  True)
-        self.assertAlmostEqual         (res['delta'],                -0.15887364780635896)
-        value025 = find_list_of_dicts_element(res['confidence_interval'], 'percentile',  2.5, 'value')
-        value975 = find_list_of_dicts_element(res['confidence_interval'], 'percentile', 97.5, 'value')
-        np.testing.assert_almost_equal (value025,                     -0.24461812530841959, decimal=5)
-        np.testing.assert_almost_equal (value975,                     -0.07312917030429833, decimal=5)
-        self.assertEqual               (res['treatment_sample_size'],  1000)
-        self.assertEqual               (res['control_sample_size'],    1000)
-        self.assertAlmostEqual         (res['treatment_mean'],        -0.045256707490195384)
-        self.assertAlmostEqual         (res['control_mean'],           0.11361694031616358)
+        self.assertEqual(res.treatment_statistics.sample_size,     1000)
+        self.assertEqual(res.control_statistics.sample_size,       1000)
+        self.assertAlmostEqual(res.treatment_statistics.mean,     -0.045256707490195384)
+        self.assertAlmostEqual(res.control_statistics.mean,        0.11361694031616358)
+        self.assertAlmostEqual(res.treatment_statistics.variance,  0.9742344563121542)
+        self.assertAlmostEqual(res.control_statistics.variance,    0.9373337542827797)
 
-    def test_group_sequential_multi_test_correction(self):
-        """
-        Test group sequential with multiple correction
-        """
-        res = es.group_sequential(self.rand_s1, self.rand_s2, multi_test_correction=True, num_tests=25)
+        self.assertAlmostEqual(res.delta, -0.15887364780635896)
+        value025 = find_list_of_dicts_element(res.confidence_interval, 'percentile', 2.5, 'value')
+        value975 = find_list_of_dicts_element(res.confidence_interval, 'percentile', 97.5, 'value')
+        np.testing.assert_almost_equal(value025, -0.24461812530841959, decimal=5)
+        np.testing.assert_almost_equal(value975, -0.07312917030429833, decimal=5)
 
-        self.assertEqual(res['stop'], True)
-        self.assertAlmostEqual(res['delta'], -0.15887364780635896)
-        value025 = find_list_of_dicts_element(res['confidence_interval'], 'percentile', 0.1, 'value')
-        value975 = find_list_of_dicts_element(res['confidence_interval'], 'percentile', 99.9, 'value')
-        np.testing.assert_almost_equal(value025, -0.29416175390519078, decimal=5)
-        np.testing.assert_almost_equal(value975, -0.023585541707525692, decimal=5)
-        self.assertEqual(res['treatment_sample_size'], 1000)
-        self.assertEqual(res['control_sample_size'], 1000)
-        self.assertAlmostEqual(res['treatment_mean'], -0.045256707490195384)
-        self.assertAlmostEqual(res['control_mean'], 0.11361694031616358)
+        self.assertAlmostEqual(res.p,                 0.0002863669955157941)
+        self.assertAlmostEqual(res.statistical_power, 0.9529152504960496)
+        self.assertEqual(res.stop,                    True)
 
     def test_group_sequential_actual_size_larger_than_estimated(self):
-        """
-        Check the group sequential function with wrong input,
+        """ Check the group sequential function with wrong input,
         such that the actual data size is already larger than estimated sample size.
         """
         res = es.group_sequential(self.rand_s1, self.rand_s2, estimated_sample_size=100)
 
-        value025 = find_list_of_dicts_element(res['confidence_interval'], 'percentile',  2.5, 'value')
-        value975 = find_list_of_dicts_element(res['confidence_interval'], 'percentile', 97.5, 'value')
-        np.testing.assert_almost_equal (value025,                     -0.24461812530841959, decimal=5)
-        np.testing.assert_almost_equal (value975,                     -0.07312917030429833, decimal=5)
+        value025 = find_list_of_dicts_element(res.confidence_interval, 'percentile',  2.5, 'value')
+        value975 = find_list_of_dicts_element(res.confidence_interval, 'percentile', 97.5, 'value')
+        np.testing.assert_almost_equal (value025, -0.24461812530841959, decimal=5)
+        np.testing.assert_almost_equal (value975, -0.07312917030429833, decimal=5)
 
 
 class BayesFactorTestCases(EarlyStoppingTestCase):
-    """
-      Test cases for the bayes_factor function in core.early_stopping.
-      """
+    """ Test cases for the bayes_factor function in core.early_stopping."""
 
     # @unittest.skip("sometimes takes too much time")
     def test_bayes_factor(self):
-        """
-        Check the Bayes factor function.
-        """
+        """ Check the Bayes factor function."""
         res = es.bayes_factor(self.rand_s1, self.rand_s2, num_iters=2000)
 
-        self.assertEqual       (res['stop'],                  True)
-        self.assertAlmostEqual (res['delta'],                -0.15887364780635896)
-        value025 = find_list_of_dicts_element(res['confidence_interval'], 'percentile',  2.5, 'value')
-        value975 = find_list_of_dicts_element(res['confidence_interval'], 'percentile', 97.5, 'value')
-        self.assertAlmostEqual (value025,                     -0.24293384641452503)
-        self.assertAlmostEqual (value975,                     -0.075064346336461404)
-        self.assertEqual       (res['treatment_sample_size'],  1000)
-        self.assertEqual       (res['control_sample_size'],    1000)
-        self.assertAlmostEqual (res['treatment_mean'],        -0.045256707490195384)
-        self.assertAlmostEqual (res['control_mean'],           0.11361694031616358)
+        self.assertEqual(res.treatment_statistics.sample_size,     1000)
+        self.assertEqual(res.control_statistics.sample_size,       1000)
+        self.assertAlmostEqual(res.treatment_statistics.mean,     -0.045256707490195384)
+        self.assertAlmostEqual(res.control_statistics.mean,        0.11361694031616358)
+        self.assertAlmostEqual(res.treatment_statistics.variance,  0.9742344563121542)
+        self.assertAlmostEqual(res.control_statistics.variance,    0.9373337542827797)
+
+        self.assertAlmostEqual(res.delta, -0.15887364780635896)
+        value025 = find_list_of_dicts_element(res.confidence_interval, 'percentile', 2.5, 'value')
+        value975 = find_list_of_dicts_element(res.confidence_interval, 'percentile', 97.5, 'value')
+        np.testing.assert_almost_equal(value025, -0.24293384641452503, decimal=5)
+        np.testing.assert_almost_equal(value975, -0.075064346336461404, decimal=5)
+        self.assertEqual(res.p,                 None)
+        self.assertEqual(res.statistical_power, None)
+        self.assertEqual(res.stop,              True)
 
     # @unittest.skip("sometimes takes too much time")
     def test_bayes_factor_poisson(self):
-        """
-        Check the Bayes factor function for Poisson distributions.
-        """
-        res= es.bayes_factor(self.rand_s3, self.rand_s4, distribution='poisson', num_iters=2000)
-        self.assertEqual       (res['stop'],                  True)
-        self.assertAlmostEqual (res['delta'],                -1.9589999999999999)
-        value025 = find_list_of_dicts_element(res['confidence_interval'], 'percentile',  2.5, 'value')
-        value975 = find_list_of_dicts_element(res['confidence_interval'], 'percentile', 97.5, 'value')
-        self.assertAlmostEqual (value025,                     -2.0713281392132465)
-        self.assertAlmostEqual (value975,                     -1.8279692168150592)
-        self.assertEqual       (res['treatment_sample_size'],  1000)
-        self.assertEqual       (res['control_sample_size'],    1000)
-        self.assertAlmostEqual (res['treatment_mean'],         0.96599999999999997)
-        self.assertAlmostEqual (res['control_mean'],           2.9249999999999998)
+        """ Check the Bayes factor function for Poisson distributions."""
+        res = es.bayes_factor(self.rand_s3, self.rand_s4, distribution='poisson', num_iters=2000)
+
+        self.assertEqual(res.treatment_statistics.sample_size,      1000)
+        self.assertEqual(res.control_statistics.sample_size,        1000)
+        self.assertAlmostEqual(res.treatment_statistics.mean,       0.96599999999999997)
+        self.assertAlmostEqual(res.control_statistics.mean,         2.9249999999999998)
+        self.assertAlmostEqual(res.treatment_statistics.variance,   0.868844)
+        self.assertAlmostEqual(res.control_statistics.variance,     2.901375)
+
+        self.assertAlmostEqual(res.delta, -1.9589999999999999)
+        value025 = find_list_of_dicts_element(res.confidence_interval, 'percentile', 2.5, 'value')
+        value975 = find_list_of_dicts_element(res.confidence_interval, 'percentile', 97.5, 'value')
+        np.testing.assert_almost_equal(value025, -2.0713281392132465, decimal=5)
+        np.testing.assert_almost_equal(value975, -1.8279692168150592, decimal=5)
+        self.assertEqual(res.p,                 None)
+        self.assertEqual(res.statistical_power, None)
+        self.assertEqual(res.stop,              True)
 
     # @unittest.skip("sometimes takes too much time")
     def test_bayes_factor_with_nan_input(self):
-        """
-        Check the Bayes factor function with input that contains nan values.
-        """
-        res= es.bayes_factor(self.rand_s5, self.rand_s6, num_iters=2000)
-        self.assertEqual(res['stop'], True)
+        """ Check the Bayes factor function with input that contains nan values."""
+        res = es.bayes_factor(self.rand_s5, self.rand_s6, num_iters=2000)
+        self.assertEqual(res.stop, True)
 
     def test_variational_inference(self):
-        """
-        Check bayesian sampling using variational bayes.
-        """
-        traces, n_x, n_y, mu_x, mu_y = es._bayes_sampling(self.rand_s1, self.rand_s2, num_iters=2000, inference="variational")
-
+        """ Check bayesian sampling using variational bayes."""
+        traces, n_x, n_y, mu_x, mu_y = es._bayes_sampling(self.rand_s1, self.rand_s2,
+                                                          num_iters=2000, inference="variational")
         self.assertEqual(len(traces), 4)
         self.assertEqual(len(traces['delta']), 1001)
         self.assertEqual(n_x, 1000)
@@ -162,28 +144,28 @@ class BayesFactorTestCases(EarlyStoppingTestCase):
 
 
 class BayesPrecisionTestCases(EarlyStoppingTestCase):
-    """
-      Test cases for the bayes_precision function in core.early_stopping.
-      """
+    """ Test cases for the bayes_precision function in core.early_stopping."""
 
     # @unittest.skip("sometimes takes too much time")
     def test_bayes_precision(self):
-        """
-        Check the bayes_precision function.
-        """
+        """ Check the bayes_precision function."""
         res = es.bayes_precision(self.rand_s1, self.rand_s2, num_iters=2000)
 
-        self.assertEqual       (res['stop'],                  0)
-        self.assertAlmostEqual (res['delta'],                -0.15887364780635896)
-        value025 = find_list_of_dicts_element(res['confidence_interval'], 'percentile',  2.5, 'value')
-        value975 = find_list_of_dicts_element(res['confidence_interval'], 'percentile', 97.5, 'value')
-        self.assertAlmostEqual (value025,                     -0.24293384641452503)
-        self.assertAlmostEqual (value975,                     -0.075064346336461404)
-        self.assertEqual       (res['treatment_sample_size'],  1000)
-        self.assertEqual       (res['control_sample_size'],    1000)
-        self.assertAlmostEqual (res['treatment_mean'],         -0.045256707490195384)
-        self.assertAlmostEqual (res['control_mean'],           0.11361694031616358)
+        self.assertEqual(res.treatment_statistics.sample_size, 1000)
+        self.assertEqual(res.control_statistics.sample_size, 1000)
+        self.assertAlmostEqual(res.treatment_statistics.mean, -0.045256707490195384)
+        self.assertAlmostEqual(res.control_statistics.mean, 0.11361694031616358)
+        self.assertAlmostEqual(res.treatment_statistics.variance, 0.9742344563121542)
+        self.assertAlmostEqual(res.control_statistics.variance, 0.9373337542827797)
 
+        self.assertAlmostEqual(res.delta, -0.15887364780635896)
+        value025 = find_list_of_dicts_element(res.confidence_interval, 'percentile', 2.5, 'value')
+        value975 = find_list_of_dicts_element(res.confidence_interval, 'percentile', 97.5, 'value')
+        np.testing.assert_almost_equal(value025, -0.24293384641452503, decimal=5)
+        np.testing.assert_almost_equal(value975, -0.07506434633646140, decimal=5)
+        self.assertEqual(res.p,                 None)
+        self.assertEqual(res.statistical_power, None)
+        self.assertEqual(res.stop,              False)
 
 if __name__ == '__main__':
     unittest.main()