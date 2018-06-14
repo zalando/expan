Now pass 'ddof=1' to np.var and np.std within 'fixed_horizon'. This is to ensure our pooled_std is correct. For large samples, this change is negligible

expan/core/statistics.py
@@ -389,8 +389,8 @@ def normal_sample_weighted_difference(x_numerators, y_numerators, x_denominators
     mean2 = np.mean(y_numerators) / np.mean(y_denominators)
     errors_1 = x_numerators - (x_denominators * mean1)
     errors_2 = y_numerators - (y_denominators * mean2)
-    std1 = np.std(errors_1 / np.mean(x_denominators))
-    std2 = np.std(errors_2 / np.mean(y_denominators))
+    std1 = np.std(errors_1 / np.mean(x_denominators), ddof=1)
+    std2 = np.std(errors_2 / np.mean(y_denominators), ddof=1)
     n1 = len(_x_ratio)
     n2 = len(_y_ratio)
 
@@ -406,8 +406,8 @@ def normal_sample_weighted_difference(x_numerators, y_numerators, x_denominators
             ,   'mean2': mean2
             ,   'n1': n1
             ,   'n2': n2
-            ,   'var1': np.var(errors_1 / np.mean(x_denominators))
-            ,   'var2': np.var(errors_2 / np.mean(y_denominators))
+            ,   'var1': np.var(errors_1 / np.mean(x_denominators), ddof=1)
+            ,   'var2': np.var(errors_2 / np.mean(y_denominators), ddof=1)
             ,   'p_value': p_value
             }
 

tests/test_derived.py
@@ -52,8 +52,9 @@ def very_compact_test(  expected_results,
     assert tup == tuple_approx(*expected_results)
 
 def test_derived_fixed_horizon_equalweights():
-    very_compact_test(          (-1.0,0.207999999,0.322771,None,(3.0,3,0.6666666)
-                                ,(2.0,3,0.66666666),((2.5,-2.8509634),(97.5,0.8509634))),
+    very_compact_test(          (   -1.0,0.2878641347266908,0.322771,None,
+                                    (3.0,3,1.0),(2.0,3,1.0),
+                                    ((2.5,-3.266957935527524),(97.5,1.2669579355275231))),
                                 'fixed_horizon',
                                 [], {'min_observations':0},
                                 np.array([1,2,3]),
@@ -63,8 +64,9 @@ def test_derived_fixed_horizon_equalweights():
                                 )
 
 def test_derived_fixed_horizon_equalweights_not_1():
-    very_compact_test(          (-1.0,0.207999999,0.322771,None,(3.0,3,0.6666666)
-                                ,(2.0,3,0.66666666),((2.5,-2.8509634),(97.5,0.8509634))),
+    very_compact_test(          (   -1.0,0.2878641347266908,0.322771,None,
+                                    (3.0,3,1.0),(2.0,3,1.0),
+                                    ((2.5,-3.266957935527524),(97.5,1.2669579355275231))),
                                 'fixed_horizon',
                                 [], {'min_observations':0},
                                 np.array([1,2,3]),