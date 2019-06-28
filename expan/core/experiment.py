64633dd9d408bc69a5f7914311da756c19ce52c0
Grigory Bordyugov <gregbg@gmail.com>
2019-06-28 17:11:30 +0200

    some cleanup

--- expan/core/experiment.py
+++ expan/core/experiment.py
@@ -309,28 +309,27 @@ class Experiment(object):
             return data.apply(lambda x: x > threshold)
 
         def find_smallest_and_largest(data, quantile):
-            print(data)
             rest = 1.0 - quantile
             quantiles = [rest/2.0, 1.0 - rest/2.0]
-            print(quantiles)
             thresholds = list(data.quantile(quantiles))
-            print(thresholds)
             return data.apply(lambda x: x < thresholds[0] or x > thresholds[1])
 
         quantile = float(percentile)/100.0
+
+        method_table = {'upper': find_largest,
+                        'lower': find_smallest,
+                        'two-sided': find_smallest_and_largest}
 
         for col in data[kpis].columns:
             column = data[col].copy()
             column.replace([np.inf, -np.inf], np.nan)
 
-            if threshold_type == 'upper':
-                flags = flags | find_largest(column, quantile)
-            elif threshold_type == 'lower':
-                flags = flags | find_smallest(column, quantile)
-            elif threshold_type == 'two-sided':
-                flags = flags | find_smallest_and_largest(column, quantile)
-            else:
+            if threshold_type not in method_table:
                 raise ValueError("Unknown outlier filtering method '%s'."%(threshold_type,))
+            else:
+                method = method_table[threshold_type]
+                flags = flags | method(column, quantile)
+
         return flags
 
     def run_goodness_of_fit_test(self, observed_freqs, expected_freqs, alpha=0.01, min_counts=5):
