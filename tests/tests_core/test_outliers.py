4c6894100c57e93112ed11540817a67540066b98
Grigory Bordyugov <gregbg@gmail.com>
2019-07-01 09:33:32 +0200

    add more tests

--- tests/tests_core/test_outliers.py
+++ tests/tests_core/test_outliers.py
@@ -3,7 +3,7 @@ from expan.core.experiment import Experiment
 import pandas as pd
 import numpy as np
 
-def test_quantile_filtering_upper():
+def test_quantile_filtering_upper_old():
     exp = Experiment({})
     data = np.array([0,0,1,2]) / np.array([0,0,1,1])
     df = pd.DataFrame.from_dict({'earnings' : data})
@@ -12,13 +12,32 @@ def test_quantile_filtering_upper():
     assert flags.tolist() == [False, False, False, True]
 
 
-def test_quantile_filtering_lower():
+def test_quantile_filtering_lower_old():
     exp = Experiment({})
     data = np.array([0,0,1,2]) / np.array([0,0,1,1])
     df = pd.DataFrame.from_dict({'earnings' : data})
 
     flags = exp._quantile_filtering(df, ['earnings'], 10, 'lower')
     assert flags.tolist() == [False, False, True, False]
+
+
+def test_quantile_filtering_upper():
+    exp = Experiment({})
+    data = np.array([0.0]*2 + range(10)) / np.array([0.0]*2 + [1.0]*10)
+    df = pd.DataFrame.from_dict({'earnings' : data})
+
+    flags = exp._quantile_filtering(df, ['earnings'], 90, 'upper')
+    assert flags.tolist() == [False]*11 + [True]
+
+
+def test_quantile_filtering_lower():
+    exp = Experiment({})
+    data = np.array([0.0]*2 + range(10)) / np.array([0.0]*2 + [1.0]*10)
+    df = pd.DataFrame.from_dict({'earnings' : data})
+
+    flags = exp._quantile_filtering(df, ['earnings'], 50, 'lower')
+    print(flags.tolist())
+    assert flags.tolist() == [False]*2 + [True]*5 + [False]*5
 
 
 def test_quantile_filtering_two_sided():
